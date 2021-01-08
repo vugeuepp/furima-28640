2d0dc97af75faca52e22b108a55141debd9bf15e
vugeuepp <vugeuepp@gmail.com>
2021-01-08 20:44:34 +0900

    ActiveHashの導入、記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,15 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping
+  belongs_to :prefecture
+  belongs_to :day
+
+  validates :item_name, :about, :price, presence: true
+
+  validates :category_id, :condition_id, :shipping_id, :prefecture_id, :day_id, numericality: { other_than: 1 } 
+
 end
