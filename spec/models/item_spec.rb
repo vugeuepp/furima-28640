2d3cca7d7b1f6a42e9bcfd0b956474856b7d1768
vugeuepp <vugeuepp@gmail.com>
2021-01-12 23:46:44 +0900

    Itemモデルのテストコード

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,80 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @item = FactoryBot.build(:item)
+    end
+
+    it "商品画像、商品名、商品の説明、カテゴリー、商品の状態、配送料の負担、発送元の地域、発送までの日数、販売価格があれば出品できること" do
+      expect(@item).to be_valid
+    end
+
+    it "画像なしでは出品できないこと" do
+      @item.image = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Image can't be blank")
+    end
+
+    it "商品名が空では出品できないこと" do
+      @item.item_name = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Item name can't be blank")
+    end 
+
+    it "商品説明が空では出品できないこと" do
+      @item.about = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("About can't be blank")
+    end 
+
+    it "カテゴリーが「---」では出品できないこと" do
+      @item.category_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category Select")
+    end 
+
+    it "商品の状態が「---」では出品できないこと" do
+      @item.condition_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Condition Select")
+    end 
+
+    it "配送料の負担が「---」では出品できないこと" do
+      @item.shipping_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Shipping Select")
+    end 
+
+    it "発送元の地域が「---」では出品できないこと" do
+      @item.prefecture_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Prefecture Select")
+    end 
+
+    it "発送までの日数が「---」では出品できないこと" do
+      @item.day_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Day Select")
+    end 
+
+    it "販売価格が空では出品できないこと" do
+      @item.price = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price can't be blank")
+    end 
+
+    it "価格の範囲が、¥300~¥9,999,999の間であること" do
+      @item.price = "37"
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price Out of setting range")
+    end
+
+    it "販売価格は半角数字のみ保存可能であること" do
+      @item.price = "３７７６"
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price Half-width number", "Price Out of setting range")
+    end
+
+  end
 end
