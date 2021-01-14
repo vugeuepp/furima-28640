d0b835c4e81b949e9c35004f8ae44fa6cf81c07c
vugeuepp <vugeuepp@gmail.com>
2021-01-14 17:16:53 +0900

    コード修正３

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -74,13 +74,13 @@ RSpec.describe Item, type: :model do
         end
     
         it '販売価格が¥299以下では出品できないこと' do
-          @item.price = '37'
+          @item.price = 37
           @item.valid?
           expect(@item.errors.full_messages).to include('Price Out of setting range')
         end
     
         it '販売価格が¥10,000,000以上では出品できないこと' do
-          @item.price = '12345678'
+          @item.price = 12345678
           @item.valid?
           expect(@item.errors.full_messages).to include('Price Out of setting range')
         end
