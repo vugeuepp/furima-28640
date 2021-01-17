abf263df5016a73effde5276e3a3b8fad6a15a2b
vugeuepp <vugeuepp@gmail.com>
2021-01-17 13:18:28 +0900

    コード修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create ]
-
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update ]
+  before_action :set_item, only: [:edit, :show]
   def index
     @items = Item.all
   end
@@ -19,11 +19,11 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+
   end
 
   def edit
-    @item = Item.find(params[:id])
+
   end
 
   def update
@@ -40,5 +40,9 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:item_name, :image, :about, :category_id, :condition_id, :shipping_id, :prefecture_id, :day_id,
                                  :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
